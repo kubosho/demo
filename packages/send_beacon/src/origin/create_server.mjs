593937c98406456766cc488e2db4c389afe17484
Shota Kubota <ta2@o2p.jp>
2022-07-21 17:58:01 +0900

    feat(craete_server): improve datetime display and added user-agent display

--- packages/send_beacon/src/origin/create_server.mjs
+++ packages/send_beacon/src/origin/create_server.mjs
@@ -4,7 +4,20 @@ import http from "http";
 
 export function createServer() {
   const server = http.createServer((req, res) => {
-    console.log(`[${new Date()}] ${req.method}: ${req.url}`);
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = `${date.getSeconds()}`.padStart(2, "0");
+
+    const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
+    const userAgent = req.headers["user-agent"];
+
+    console.log(`[${formattedDate}] ${req.method}: ${req.url}`);
+    console.log(` > ${userAgent}`);
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Cache-Control", "no-cache");
