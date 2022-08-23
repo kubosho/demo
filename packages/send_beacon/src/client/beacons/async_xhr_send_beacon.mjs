984562f71aa461ec42152224537350ab08519a4e
Shota Kubota <ta2@o2p.jp>
2022-08-23 13:58:01 +0900

    fix(beacons): explicitly specify async option

--- packages/send_beacon/src/client/beacons/async_xhr_send_beacon.mjs
+++ packages/send_beacon/src/client/beacons/async_xhr_send_beacon.mjs
@@ -1,11 +1,12 @@
 export function shimSendBeaconWithXhr({ origin, event, isPersisted }) {
   return new Promise((resolve, reject) => {
+    const async = true;
     const uri =
-      `${origin}/compat_xhr/?event=${event}&t=${Date.now()}` +
+      `${origin}/compat_xhr/?event=${event}&t=${Date.now()}&async=${async}` +
       (isPersisted !== undefined ? `&persisted=${isPersisted}` : "");
     const xhr = new XMLHttpRequest();
 
-    xhr.open("POST", uri, true);
+    xhr.open("POST", uri, { async });
     xhr.onload = resolve;
     xhr.onerror = reject;
     xhr.send();
--- packages/send_beacon/src/client/beacons/sync_xhr_send_beacon.mjs
+++ packages/send_beacon/src/client/beacons/sync_xhr_send_beacon.mjs
@@ -1,9 +1,10 @@
 export function shimSendBeaconWithXhrSync({ origin, event, isPersisted }) {
+  const async = false;
   const uri =
-    `${origin}/compat_xhr_sync/?event=${event}&t=${Date.now()}` +
+    `${origin}/compat_xhr_sync/?event=${event}&t=${Date.now()}&async=${async}` +
     (isPersisted !== undefined ? `&persisted=${isPersisted}` : "");
 
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", uri, true);
+  xhr.open("POST", uri, { async });
   xhr.send();
 }
